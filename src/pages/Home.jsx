d65e92b867cc042572e99833e2aeb4b15153bfc3
rjtech-code <rjtechtop5@gmail.com>
2026-09-18 11:19:10 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -89,7 +89,7 @@ export default function Home() {
 
       <BootcampStartups />
 
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
       {/* =========================
           LATEST STORIES
       ========================= */}
